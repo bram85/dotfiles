f05851cb49d33a33d088b2f33075b3a49214ce00
Bram Schoenmakers <me@bramschoenmakers.nl>
2010-09-02 19:35:41 +0200

    Disable client list when right clicking taskbar item.

--- .config/awesome/my_panel.lua
+++ .config/awesome/my_panel.lua
@@ -46,15 +46,6 @@ mytasklist.buttons = awful.util.table.join(
     c:raise()
   end ),
 
-  awful.button( { }, 3, function()
-    if instance then
-      instance:hide()
-      instance = nil
-    else
-      instance = awful.menu.clients({ width=250 })
-    end
-  end ),
-
   awful.button( { }, 4, function()
     awful.client.focus.byidx(1)
     if client.focus then
