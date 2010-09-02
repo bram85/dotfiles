a4dc1223a94a0d921b7ac32942030ac08c0bec47
Bram Schoenmakers <me@bramschoenmakers.nl>
2010-09-02 19:18:51 +0200

    Remove one layout I wouldn't use anyway.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -37,7 +37,6 @@ layouts =
   awful.layout.suit.fair,
   awful.layout.suit.fair.horizontal,
   awful.layout.suit.spiral,
-  awful.layout.suit.spiral.dwindle,
   awful.layout.suit.max,
   awful.layout.suit.max.fullscreen,
   awful.layout.suit.magnifier
