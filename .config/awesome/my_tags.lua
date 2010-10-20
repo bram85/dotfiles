56933d6f60d74ba941f05d8875adf492e40b3317
Bram Schoenmakers <me@bramschoenmakers.nl>
2010-10-20 11:59:12 +0200

    Remove configuration for a host.
    I don't use awesome on 'jupiter' anymore.

--- .config/awesome/my_tags.lua
+++ .config/awesome/my_tags.lua
@@ -3,30 +3,16 @@
 tags = {}
 for s = 1, screen.count() do
   -- Each screen has its own tag table.
-  if ( hostname == "jupiter" ) then
-    tags[s] = awful.tag( {
-      "1 www",
-      "2 rdp",
-      "3 term",
-      "4 work1",
-      "5 work2",
-      6,
-      7,
-      8,
-      9
-    }, s, layouts[ 2 ] )
-  else
-    tags[s] = awful.tag( {
-      "1 www",
-      "2 chat",
-      "3 term",
-      "4 work1",
-      "5 work2",
-      "6 music",
-      7,
-      8,
-      9
-    }, s, layouts[ 2 ] )
-  end
+  tags[s] = awful.tag( {
+    "1 www",
+    "2 chat",
+    "3 term",
+    "4 work1",
+    "5 work2",
+    "6 music",
+    7,
+    8,
+    9
+  }, s, layouts[ 2 ] )
 end
 -- }}}
