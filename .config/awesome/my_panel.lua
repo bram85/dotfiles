df2d33f6f4f62ab729edcd9767c440d81f91141a
Bram Schoenmakers <me@bramschoenmakers.nl>
2011-04-16 11:12:59 +0200

    Refresh clock every 15 seconds.

--- .config/awesome/my_panel.lua
+++ .config/awesome/my_panel.lua
@@ -44,7 +44,7 @@ appbuttons = {
 -- {{{ Wibox
 
 -- Create a textclock widget
-mytextclock = awful.widget.textclock( { align = "right" }, "%a %d %b, %H:%M", 1 )
+mytextclock = awful.widget.textclock( { align = "right" }, "%a %d %b, %H:%M", 15 )
 mytextclock:buttons( awful.util.table.join(
   awful.button( { }, 1, function()
     local cs = client.get()
